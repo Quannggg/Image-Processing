3ccda681cb5a48e5a698a1dc24c901e8f7a628ff
Quannggg <nhieuvai@gmail.com>
2024-03-29 16:42:50 +0700

    Update HW1

--- HW1/ex1.py
+++ HW1/ex1.py
@@ -96,6 +96,9 @@ if __name__ == "__main__":
 
     # Display the flipped grayscale image
     display_image(img_gray_flipped, "Flipped Grayscale Image")
+    
+    #save the flipped grayscale image
+    save_image(img_gray_flipped, "images/lena_gray_flipped.jpg")
 
     # Rotate the grayscale image
     img_gray_rotated = rotate_image(img_gray, 45)
